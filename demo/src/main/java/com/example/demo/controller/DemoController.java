2ba5bf8eaf2c36b498aa8534d614380d42a8c1b2
priyanshu <priyanshusharma0152@gmail.com>
2025-05-02 12:52:10 +0530

    Login Feature

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class DemoController{
+	String name=null,id=null;
+	@RequestMapping("/name")
+	public String getName(@RequestParam("un") String un) {
+		name=un;
+		return "Name is saved as "+name;
+	}
+	@RequestMapping("/id")
+	public String getId(@RequestParam("ui") String ui) {
+		id=ui;
+		return "ID is saved as "+ui;
+	}
+	@RequestMapping("/info")
+	public String getId() {
+		return "Name of the user is "+name+" and Id of the user is "+id;
+	}
+	
+}
